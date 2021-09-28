6f91b8504e788be46c368e9723ff8b8e06b22038
Yocxan Maximilian <86076747+y0cxan@users.noreply.github.com>
2021-09-28 14:37:54 +0700

    website
    galeri yocxan

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Yocxan</title>
+<link rel="stylesheet" href="baru.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
+
+  <section class="header">
+    <nav>
+      <a href=""><img src="img/logoku.png"></a>
+    </nav>
+<img src="img/diri.png" class="diri">
+     <div class="text-box">
+       <h1>YOCXAN</h1>
+       <p>Making design is now one of the easiest thing in the world. You just need to learn ilustrator or other software design and you are good to go.</p>
+       <form class="box" action="awal.html">
+       <button type="submit" name="submit" class="hero-btn">Explore</button>
+       </form>
+     </div>
+  </section>
+
+</body>
+</html>
